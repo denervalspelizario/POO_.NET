3e38524ee540f6f23bf88cc45d4a0f09cbee5aef
Denerval s Pelizario <denervalspelizario@gmail.com>
2024-04-19 11:34:55 -0300

    estudo c# lista tipo hashset

---
+++ HashSet/HashSet/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("## HashSet ##");
+
+// criação coleção hashset com itens
+var numeros = new HashSet<int>() { 1,2,3,4,5};
+
+
+var frutas = new HashSet<string>() { "abacaxi", "melao", "melancia", "uva"};
+
+
+HashSet<int> pares = new HashSet<int>();
+
+pares.Add(2);
+pares.Add(4);
+pares.Add(6);
+pares.Add(8);
+
+foreach (var i in pares)
+{
+    Console.WriteLine(i);
+}
+
+
+
+Console.ReadKey();
